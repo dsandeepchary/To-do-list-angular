26288b6ec633f58fa6d8d9ab0617adcd40457b30
root <root@localhost.localdomain>
2018-05-30 23:32:16 +0530

    Fixing main height

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
     headingClass = 'text-light';
     addClass = 'btn btn-outline-light night-color-hover rounded-0 border-0';
     tickClass = 'night-color-hover btn btn-outline-light rounded-0 border-0';
-    mainDivClass = 'night-mode';
+    mainClass = 'night-mode';
     adjustClass = 'btn night-color-hover btn-outline-light rounded-0 border-0 btn-lg pull-right';
 
     addNewInput() {
@@ -35,20 +35,20 @@ export class AppComponent {
 
     toggleMode() {
         this.isNightMode = !this.isNightMode;
-        if (this.isNightMode === false) {
-            this.importantClass = 'btn btn-outline-dark imp-day rounded-0 border-0';
-            this.headingClass = 'text-dark';
-            this.addClass = 'btn btn-outline-dark day-color-hover rounded-0 border-0';
-            this.tickClass = 'day-color-hover btn btn-outline-dark rounded-0 border-0';
-            this.mainDivClass = 'day-mode';
-            this.adjustClass = 'btn day-color-hover btn-outline-dark rounded-0 border-0 btn-lg pull-right';
-        } else {
+        if (this.isNightMode === true) {
             this.headingClass = 'text-light';
             this.importantClass = 'btn btn-outline-light imp-night rounded-0 border-0';
             this.addClass = 'btn btn-outline-light night-color-hover rounded-0 border-0';
             this.tickClass = 'night-color-hover btn btn-outline-light rounded-0 border-0';
-            this.mainDivClass = 'night-mode';
+            this.mainClass = 'night-mode';
             this.adjustClass = 'btn night-color-hover btn-outline-light rounded-0 border-0 btn-lg pull-right';
+        } else {
+            this.importantClass = 'btn btn-outline-dark imp-day rounded-0 border-0';
+            this.headingClass = 'text-dark';
+            this.addClass = 'btn btn-outline-dark day-color-hover rounded-0 border-0';
+            this.tickClass = 'day-color-hover btn btn-outline-dark rounded-0 border-0';
+            this.mainClass = 'day-mode';
+            this.adjustClass = 'btn day-color-hover btn-outline-dark rounded-0 border-0 btn-lg pull-right';
         }
     }
 }
